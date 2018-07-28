90396bb5e40e39f95f7d6fd281bc5d5fc9903b65
Bratosin Alexandru <alex.bratosin01@gmail.com>
2018-07-28 21:07:52 +0300

    Fix system group request command typo

--- src/commands/update_loop_email.py
+++ src/commands/update_loop_email.py
@@ -39,7 +39,7 @@ class UpdateLoopEmail(RecurrentAction):
 
 			# Gather system info
 			message += '# System\n'
-			system = self.projector.get_system_group()
+			system = self.projector.request_system_group()
 			for s in system:
 				message += '\t{}: {}\n'.format(s, system[s])
 			message += '\n'
